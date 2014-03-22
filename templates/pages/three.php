ed84ae95f34a467dc58e264470122c2206bbdd02
Tyler Carter <tcarter@appthemes.com>
2014-03-22 08:26:41 -0700

    Update answer to point to 29th fibonacci number

--- templates/pages/three.php
+++ templates/pages/three.php
@@ -3,4 +3,4 @@
 <p>The next 10 terms would be:</p>
 <pre>0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89</pre>
 <p>In this case the 6th fibonacci number is 5.</p>
-<p>Find the 30th.</p>
+<p>Find the 29th.</p>
