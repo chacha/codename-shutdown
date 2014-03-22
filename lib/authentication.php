7ee6900eba9ff96b73febb2e90f55fdcd1363eff
Tyler Carter <tcarter@appthemes.com>
2014-03-21 22:34:24 -0700

    Add terminal and authentication system

---
+++ lib/authentication.php
@@ -0,0 +1,62 @@
+<?php 
+
+class Authentication{
+
+	private static $is_logged_in = false;
+	private static $user = array();
+
+	public static function init(){
+
+		session_start();
+
+		if( isset( $_SESSION['user'] ) ){
+			self::$user = $_SESSION['user'];
+			self::$is_logged_in = true;
+		}
+
+	}
+
+	public static function set_user( $name, $level ) {
+		$_SESSION['user'] = array(
+			'name' => $name,
+			'level' => $level
+		);
+	}
+
+	public static function clear_user( ) {
+
+		unset( $_SESSION['user'] );
+		self::$user = array();
+		self::$is_logged_in = false;
+
+	}
+
+	public static function get_user(){
+		if( ! self::$is_logged_in )
+			return false;
+
+		return self::$user;
+	}
+
+	public static function get_name(){
+		if( ! self::$is_logged_in )
+			return false;
+
+		return self::$user['name'];	
+	}
+	
+	public static function is_admin(){
+		if( ! self::$is_logged_in )
+			return false;
+		
+		if( self::$user['level'] == 'admin' )
+			return true;
+		else
+			return false;
+	}
+
+	public static function is_logged_in(){
+		return self::$is_logged_in;
+	}
+
+}
---
+++ lib/database.php
@@ -0,0 +1,84 @@
+<?php 
+
+class Database
+{
+
+	private $mysql = array();
+	private $connection;
+        private $log;
+	
+	// MySQL Error Numbers
+	const ERROR_DB_NOT_FOUND = 1049; // Database Not Found
+	
+	public function __construct($mysql_host, $mysql_username, $mysql_password, $mysql_database="")
+	{
+		$this->mysql = array("host" => $mysql_host,
+					 "username" => $mysql_username,
+					 "password" => $mysql_password,
+					 "database" => $mysql_database
+		);
+	}
+	
+	public function connect()
+	{
+		$connection = mysql_connect($this->mysql["host"],
+                                            $this->mysql["username"],
+                                            $this->mysql["password"]
+		);
+		echo mysql_error();
+		// Check that the database connected
+		if(!is_resource($connection))
+		{
+			// Throw the Exception
+			throw new Exception("Database Connection Unavailable", 500);
+		}
+                
+		$this->connection = $connection;	
+		
+		if(true != empty($this->mysql["database"]))
+		{
+			// Check that the Database Exists
+			$this->select_db($this->mysql["database"]);
+			if(mysql_errno() == self::ERROR_DB_NOT_FOUND)
+			{
+				// Database Setup Error (Default Database Not Defined)
+				throw new Exception("Selected Database Not Defined", 404);
+			}
+		}
+	}
+	
+	public function select_db($database_name)
+	{
+		mysql_select_db($this->mysql["database"], $this->connection);
+	}
+
+	public function query($sql)
+	{
+		if(!is_resource($this->connection))
+		{
+			$this->connect();
+		}
+		
+		$resource = mysql_query($sql,$this->connection);
+		
+		if(mysql_error())
+		{
+			return mysql_error();
+		}
+		
+		if(is_resource($resource))
+		{
+                    $return = array();
+                    while($temp_row = mysql_fetch_assoc($resource))
+                    {
+                            $return[] = $temp_row;
+                    }
+		}
+		else
+		{
+			$return = $resource;
+		}
+		return $return;
+	}	
+}
+?>
